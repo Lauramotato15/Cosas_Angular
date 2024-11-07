6242ef3bc74f42b543f2e01b41d0716d4fcecfd1
Laura Motato <morenocaterin988@gmail.com>
2024-11-07 17:11:15 -0500

    Avance en el proyecto prueba

--- Prueba_Angular/src/app/dbz/pages/main-page.component.ts
+++ Prueba_Angular/src/app/dbz/pages/main-page.component.ts
@@ -1,8 +1,20 @@
 import { Component} from '@angular/core';
+import { Character } from '../interface/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-pages.component.html'
 })
 
-export class MainPageComponent {}
+export class MainPageComponent {
+  public arrayCharacter :Character[] = [
+    {
+      name: "Krillin",
+      power: 27
+    },
+    {
+      name: "Goku",
+      power: 12
+    }
+  ];
+}
