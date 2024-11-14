d94a1939c481322023ef1533899e6ae10b53b88a
Laura Motato <morenocaterin988@gmail.com>
2024-11-14 14:25:35 -0500

    Segundo proyecto de practica terminado

--- demoApp/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ demoApp/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -6,13 +6,24 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './lazy-image.component.css'
 })
 export class LazyImageComponent implements OnInit{
+  private loadI:boolean = false;
+
   @Input()
   public url!:string;
 
   @Input()
   public alt!:string
 
+  get load():boolean{
+    return this.loadI;
+  }
+
   ngOnInit(): void {
     if(!this.url || !this.alt)throw new Error('is property required')
+  }
+
+  loadImage(){
+    this.loadI = true;
+    console.log(this.loadI);
   }
 }
